e496b3431eb523e873d3ed3a0ad460284c2c0127
AndreiTheFox <andrewicehab@gmail.com>
2023-10-31 20:41:42 +0700

    Homework Commit. Custom View.

---
+++ app/src/main/java/ru/netology/nmedia/ui/AppActivity.kt
@@ -0,0 +1,29 @@
+package ru.netology.nmedia.ui
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.R
+
+class AppActivity : AppCompatActivity(R.layout.activity_app) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        var inputData = listOf(
+            500F,
+            500F,
+            500F,
+            500F,
+            500F,
+            500F,
+            500F,
+            500F,
+        )
+        val sum = inputData.sum()
+        if (sum != 0F) findViewById<StatsView>(R.id.stats).data = inputData.map {it/sum*100F}
+        else {
+            val errorText ="No data to display"
+            Toast.makeText(this,errorText,Toast.LENGTH_LONG).show()
+        }
+    }
+}
